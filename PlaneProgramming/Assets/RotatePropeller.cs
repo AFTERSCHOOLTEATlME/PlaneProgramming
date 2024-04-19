c2763de7f3f1525f0a79ba424b7d55425c3c9c7f
Red <kaurk1@roverkids.org>
2024-04-18 21:46:13 -0400

    Controls Added

--- PlaneProgramming/Assets/RotatePropeller.cs
+++ PlaneProgramming/Assets/RotatePropeller.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class RotatePropeller : MonoBehaviour
 {
-    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-        // Move the vehicle foward
-        transform.Translate(Vector3.back * Time.deltaTime * speed);
-        
+        transform.Rotate(Vector3.forward, 40);
     }
 }
